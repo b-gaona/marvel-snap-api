00e14e9a41c0185f983cdb578b9d295fa01991f9
br-gaona <brandongaona03@hotmail.com>
2022-12-16 18:40:16 -0600

    Adding the endpoints of the API

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,10 +1,23 @@
 const express = require("express");
 
 const cardsRouter = require("./cards/cards.router");
+const decksRouter = require("./decks/decks.router");
 
 const api = express.Router();
 
+const endpoints = {
+  cards: "http://localhost:3000/v1/cards/",
+  decks: "http://localhost:3000/v1/decks/",
+}
+
 //The use of v1 is to set a version to the API
+
 api.use("/cards", cardsRouter);
+
+api.use("/decks", decksRouter);
+
+api.use("/", (req, res) => {
+  return res.status(200).json(endpoints);
+});
 
 module.exports = api;
