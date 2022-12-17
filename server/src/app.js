428a05040e104b94bc20a9b5859b7b6b3ba2ae21
br-gaona <brandongaona03@hotmail.com>
2022-12-17 14:18:36 -0600

    Changing the window to a global in the server js

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ const api = require("./routes/api");
 const app = express(helmet());
 
 app.use(cors({
-  origin: window.location.origin, //This server can do requests to the server, it's like a whitelist
+  origin: global.location.origin, //This server can do requests to the server, it's like a whitelist
 })); //To allow cross origin
 
 app.use(morgan("combined"));
