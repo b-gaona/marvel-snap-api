5c949278c934f7538da154714ee3e2e6044fdad5
br-gaona <brandongaona03@hotmail.com>
2022-12-17 14:22:15 -0600

    Changing the global to the host header

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ const api = require("./routes/api");
 const app = express(helmet());
 
 app.use(cors({
-  origin: global.location.origin, //This server can do requests to the server, it's like a whitelist
+  origin: request.headers.host, //This server can do requests to the server, it's like a whitelist
 })); //To allow cross origin
 
 app.use(morgan("combined"));
