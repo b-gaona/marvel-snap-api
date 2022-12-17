cb48dfcdff71e429a5a873ca022e186152557484
br-gaona <brandongaona03@hotmail.com>
2022-12-16 18:41:24 -0600

    Adding the loadDecksData function when the server starts

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,7 @@ const app = require("./app");
 const { mongoConnect } = require("./services/mongo");
 
 const { loadCardsData } = require("./models/cards.model");
+const { loadDecksData } = require("./models/decks.model");
 
 const PORT = process.env.PORT || 3000; //To avoid conflict with the 3000 that's using React
 
@@ -14,6 +15,7 @@ const server = http.createServer(app);
 async function startServer() {
   await mongoConnect();
   await loadCardsData();
+  await loadDecksData();
   
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
